619395a8c8ed13f780fa65d863109bacf5b9c0dd
Viktor Gorokhov <patronusv@gmail.com>
2021-02-01 04:26:34 +0200

    refactors code using redux toolkit

--- src/redux/actions/phonebookActions.js
+++ src/redux/actions/phonebookActions.js
@@ -1,26 +1,32 @@
-import { ADD_CONTACT, DELETE_CONTACT, GET_INITIAL_CONTACTS, SET_FILTER } from '../constants/phonebookConstants';
+// import { ADD_CONTACT, DELETE_CONTACT, GET_INITIAL_CONTACTS, SET_FILTER } from '../constants/phonebookConstants';
+import { createAction } from '@reduxjs/toolkit';
 
-export const addNewContact = data => {
-  return {
-    type: ADD_CONTACT,
-    payload: data,
-  };
-};
-export const deleteContact = id => {
-  return {
-    type: DELETE_CONTACT,
-    payload: id,
-  };
-};
-export const setFilter = value => {
-  return {
-    type: SET_FILTER,
-    payload: value,
-  };
-};
-export const getInitialContacts = data => {
-  return {
-    type: GET_INITIAL_CONTACTS,
-    payload: data,
-  };
-};
+export const addNewContact = createAction('phonebook/addNewContact');
+export const deleteContact = createAction('phonebook/deleteContact');
+export const setFilter = createAction('phonebook/setFilter');
+export const getInitialContacts = createAction('phonebook/getInitialContacts');
+
+// export const addNewContact = data => {
+//   return {
+//     type: ADD_CONTACT,
+//     payload: data,
+//   };
+// };
+// export const deleteContact = id => {
+//   return {
+//     type: DELETE_CONTACT,
+//     payload: id,
+//   };
+// };
+// export const setFilter = value => {
+//   return {
+//     type: SET_FILTER,
+//     payload: value,
+//   };
+// };
+// export const getInitialContacts = data => {
+//   return {
+//     type: GET_INITIAL_CONTACTS,
+//     payload: data,
+//   };
+// };
--- src/redux/reducers/phonebookReducer.js
+++ src/redux/reducers/phonebookReducer.js
@@ -1,34 +1,64 @@
-import { ADD_CONTACT, DELETE_CONTACT, SET_FILTER, GET_INITIAL_CONTACTS } from '../constants/phonebookConstants';
+import { createReducer } from '@reduxjs/toolkit';
+import { addNewContact, deleteContact, getInitialContacts, setFilter } from '../actions/phonebookActions';
+// import { ADD_CONTACT, DELETE_CONTACT, SET_FILTER, GET_INITIAL_CONTACTS } from '../constants/phonebookConstants';
 
 const initialState = {
   contacts: [],
   filter: '',
 };
 
-const phonebookReducer = (state = { ...initialState }, action) => {
-  switch (action.type) {
-    case ADD_CONTACT:
-      return {
-        ...state,
-        contacts: [...state.contacts, action.payload],
-      };
-    case DELETE_CONTACT:
-      return {
-        ...state,
-        contacts: [...state.contacts.filter(item => item.id !== action.payload)],
-      };
-    case SET_FILTER:
-      return {
-        ...state,
-        filter: action.payload,
-      };
-    case GET_INITIAL_CONTACTS:
-      return {
-        ...state,
-        contacts: [...action.payload],
-      };
-    default:
-      return state;
-  }
-};
+const onAddContact = (state, action) => ({
+  ...state,
+  contacts: [...state.contacts, action.payload],
+});
+const onDeleteContact = (state, action) => ({
+  ...state,
+  contacts: [...state.contacts.filter(item => item.id !== action.payload)],
+});
+const onSetFilter = (state, action) => ({
+  ...state,
+  filter: action.payload,
+});
+const onGetInitialContacts = (state, action) => ({
+  ...state,
+  contacts: [...action.payload],
+});
+
+const phonebookReducer = createReducer(
+  { ...initialState },
+  {
+    [addNewContact]: onAddContact,
+    [deleteContact]: onDeleteContact,
+    [setFilter]: onSetFilter,
+    [getInitialContacts]: onGetInitialContacts,
+  },
+);
 export default phonebookReducer;
+
+// const phonebookReducer = (state = { ...initialState }, action) => {
+//   switch (action.type) {
+//     case ADD_CONTACT:
+//       return {
+//         ...state,
+//         contacts: [...state.contacts, action.payload],
+//       };
+//     case DELETE_CONTACT:
+//       return {
+//         ...state,
+//         contacts: [...state.contacts.filter(item => item.id !== action.payload)],
+//       };
+//     case SET_FILTER:
+//       return {
+//         ...state,
+//         filter: action.payload,
+//       };
+//     case GET_INITIAL_CONTACTS:
+//       return {
+//         ...state,
+//         contacts: [...action.payload],
+//       };
+//     default:
+//       return state;
+//   }
+// };
+// export default phonebookReducer;
